16aed32b88ea59124e8085ebc28d02f3f71a28e4
m-joseph27 <muh.joseph27@gmail.com>
2020-05-29 22:38:37 +0700

    add login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="loginWrapper"></div>
+  <div class="loginWrapper">
+    <div class="leftSide"></div>
+    <div class="rightSide">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,6 +18,21 @@ export default {
   width: 100vw;
   height: 100vh;
   background-color: rgb(58, 58, 58);
+  display: flex;
+  .leftSide{
+    width: 55%;
+    height: 100%;
+    // background-color: #fff;
+    background-image: url('../assets/img/background.jpeg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .rightSide{
+    width: 45%;
+    height: 100%;
+    background-color: rgb(255, 255, 255);
+  }
 }
 
 </style>

74ec7afd1b89bccb1f48b21957fa8a8d510698dd
m-joseph27 <muh.joseph27@gmail.com>
2020-05-29 21:15:16 +0700

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loginWrapper"></div>
+</template>
+
+<script>
+export default {
+
+};
+
+</script>
+
+<style lang="scss" scoped>
+.loginWrapper{
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(58, 58, 58);
+}
+
+</style>

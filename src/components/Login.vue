a3f639f1801efeacd34ea767fb457124e476c7b5
m-joseph27 <muh.joseph27@gmail.com>
2020-05-30 13:15:37 +0700

    add register page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="loginWrapper">
-    <div class="leftSide"></div>
+    <div class="leftSide">
+      <header>hello</header>
+    </div>
     <div class="rightSide">
+      <div class="topSide"></div>
+      <div class="midSide"></div>
+      <div class="formSide"></div>
+      <div class="bottomSide"></div>
     </div>
   </div>
 </template>
@@ -23,15 +29,34 @@ export default {
     width: 55%;
     height: 100%;
     // background-color: #fff;
-    background-image: url('../assets/img/background.jpeg');
+    background-image:linear-gradient(rgba(0, 0, 0, 0.3),
+    rgba(0, 0, 0, 0.3)) ,url('../assets/img/background.jpeg');
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex;
   }
   .rightSide{
     width: 45%;
     height: 100%;
     background-color: rgb(255, 255, 255);
+    display: flex;
+    flex-direction: column;
+    .topSide{
+      width: 100%;
+      height: 90px;
+      background-color: aquamarine;
+    }
+    .midSide{
+      width: 100%;
+      height: 130px;
+      background-color: blueviolet;
+    }
+    .formSide{
+      width: 100%;
+      height: 300px;
+      background-color: burlywood;
+    }
   }
 }
 
---
+++ src/components/Register.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="loginWrapper">
+    <div class="leftSide"></div>
+    <div class="rightSide">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+
+</script>
+
+<style lang="scss" scoped>
+.loginWrapper{
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(58, 58, 58);
+  display: flex;
+  .leftSide{
+    width: 55%;
+    height: 100%;
+    // background-color: #fff;
+    background-image: url('../assets/img/background.jpeg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .rightSide{
+    width: 45%;
+    height: 100%;
+    background-color: rgb(255, 255, 255);
+  }
+}
+
+</style>

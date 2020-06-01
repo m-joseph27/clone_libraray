25e95dcbdc8afc97e050c108bbff4f31903ac445
m-joseph27 <muh.joseph27@gmail.com>
2020-06-01 13:50:13 +0700

    fixed carousel

--- src/components/Carrousel.vue
+++ src/components/Carrousel.vue
@@ -2,10 +2,34 @@
   <div class="carrouselWrapper">
     <div class="carousel"
       data-flickity='{ "wrapAround": true }'>
-      <div class="carousel-cell"></div>
-      <div class="carousel-cell"></div>
-      <div class="carousel-cell"></div>
-      <div class="carousel-cell"></div>
+      <div class="carousel-cell">
+        <img src="../assets/img/dilan.jpeg" alt="dilan">
+        <div class="carousel-name">
+          <header>Dilan 1990</header>
+          <footer>Pidi Baiq</footer>
+        </div>
+      </div>
+      <div class="carousel-cell">
+        <img src="../assets/img/laskar.jpeg" alt="laskar-pelangi">
+        <div class="carousel-name">
+          <header>Laskar pelangi</header>
+          <footer>Andrea Hirata</footer>
+        </div>
+      </div>
+      <div class="carousel-cell">
+        <img src="../assets/img/ubur-ubur.png" alt="ubur-ubur-lembur">
+        <div class="carousel-name">
+          <header>Ubur ubur Lembur</header>
+          <footer>Raditya Dika</footer>
+        </div>
+      </div>
+      <div class="carousel-cell">
+        <img src="../assets/img/react.jpeg" alt="">
+        <div class="carousel-name">
+          <header>React Native</header>
+          <footer>hoseph</footer>
+        </div>
+      </div>
   </div>
   </div>
 </template>
@@ -22,23 +46,52 @@ export default {
 body { font-family: sans-serif; }
 
 .carousel {
-  background: #EEE;
+  background: rgb(255, 255, 255);
 }
 
 .carousel-cell {
-  width: 50%;
+  width: 40%;
   height: 300px;
-  margin-right: 10px;
+  margin-right: 40px;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.267);
   background: #8C8;
-  border-radius: 5px;
-  counter-increment: carousel-cell;
+  border-radius: 15px;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+  img{
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    object-position: center;
+  }
+  .carousel-name{
+    background-color: rgba(0, 0, 0, 0.281);
+    position: absolute;
+    width: 100%;
+    height: 40%;
+    display: flex;
+    padding-left: 40px;
+    flex-direction: column;
+    justify-content: center;
+    header{
+      color: white;
+      font-size: 25px;
+      font-weight: bold;
+    }
+    footer{
+      color: white;
+      font-size: 15px;
+      font-weight: 500;
+    }
+  }
 }
 
 /* cell number */
 .carousel-cell:before {
   display: block;
   text-align: center;
-  content: counter(carousel-cell);
   line-height: 200px;
   font-size: 80px;
   color: white;
